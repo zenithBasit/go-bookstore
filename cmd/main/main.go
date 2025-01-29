b31fc01d0fb061a6d7e162bec61ebe676798213e
Basit Saiyed <basits@zenithive.com>
2025-01-29 11:21:40 +0530

    implemented db funcs

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"bookstore/pkg/routes"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStore(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9000", r))
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -20,3 +20,28 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	if err := db.First(b).Error; err != nil && gorm.ErrRecordNotFound == err {
+		db.Create(&b)
+	}
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(ID int64) (*Book, *gorm.DB) {
+	var Book Book
+	db := db.Where("ID=?", ID).Find(Book)
+	return &Book, db
+}
+
+func DeleteBook(ID int64) Book {
+	var Book Book
+	db.Where("ID=?", ID).Delete(Book)
+	return Book
+}
